273ca7220d1efd3ee897478c2ce8a99db45ef973
Arno Leruste <recroX91@hotmail.com>
2025-10-29 23:14:45 +0100

    WIP

--- CreaPrintApi/Controllers/ArticlesController.cs
+++ CreaPrintApi/Controllers/ArticlesController.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using CreaPrintCore.Interfaces;
-using AutoMapper;
 using CoreArticle = CreaPrintCore.Models.Article;
-using ApiArticle = CreaPrintApi.Dtos.Article;
 
 namespace CreaPrintApi.Controllers;
 
@@ -11,41 +9,39 @@ namespace CreaPrintApi.Controllers;
 public class ArticlesController : ControllerBase
 {
     private readonly IArticleService _service;
-    private readonly IMapper _mapper;
 
-    public ArticlesController(IArticleService service, IMapper mapper)
+    public ArticlesController(IArticleService service)
     {
         _service = service;
-        _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<ApiArticle>>> GetAll()
+    public async Task<ActionResult<IEnumerable<CoreArticle>>> GetAll()
     {
         var articles = await _service.GetAllAsync();
-        return Ok(_mapper.Map<IEnumerable<ApiArticle>>(articles));
+        return Ok(articles);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<ApiArticle>> GetById(int id)
+    public async Task<ActionResult<CoreArticle>> GetById(int id)
     {
         var article = await _service.GetByIdAsync(id);
         if (article == null) return NotFound();
-        return Ok(_mapper.Map<ApiArticle>(article));
+        return Ok(article);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ApiArticle>> Create(CoreArticle article)
+    public async Task<ActionResult<CoreArticle>> Create(CoreArticle article)
     {
         var created = await _service.CreateAsync(article);
-        return CreatedAtAction(nameof(GetById), new { id = created.Id }, _mapper.Map<ApiArticle>(created));
+        return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
     }
 
     [HttpGet("paged")]
-    public async Task<ActionResult<IEnumerable<ApiArticle>>> GetPaged([FromQuery] int page = 0, [FromQuery] int pageSize = 10)
+    public async Task<ActionResult<IEnumerable<CoreArticle>>> GetPaged([FromQuery] int page = 0, [FromQuery] int pageSize = 10)
     {
         var articles = await _service.GetPagedAsync(page, pageSize);
-        return Ok(_mapper.Map<IEnumerable<ApiArticle>>(articles));
+        return Ok(articles);
     }
 
     [HttpGet("count")]
